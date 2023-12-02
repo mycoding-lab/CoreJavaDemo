14ff9f1389c3f10a4aeaceb0c7548eb453bef7a9
thangadurai-developer <thangadurai.rajamanickam@gmail.com>
2023-12-02 16:07:25 +0530

    2nd commit

---
+++ CoreJavaDemo/src/com/test/demo/Test4.java
@@ -0,0 +1,10 @@
+package com.test.demo;
+
+public class Test4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome hello world 4");
+	}
+
+}
